08520fac62ef563970191c28d175d55c189ca913
James Grieve <pocojames@hotmail.com>
2020-10-16 01:20:46 -0700

    add brick class

--- src/game.js
+++ src/game.js
@@ -1,6 +1,8 @@
 import Paddle from '/src/paddle.js';
 import InputHandler from '/src/input.js';
 import Ball from "/src/ball.js";
+import Brick from "/src/brick.js";
+
 
 export default class Game {
     constructor(gameWidth, gameHeight) {
@@ -12,7 +14,11 @@ export default class Game {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
 
-        this.gameObjects = [this.ball, this.paddle];
+        let bricks = [];
+        for (let i = 0; i < 10; i++) {
+            bricks.push(new Brick(this, {x: i *52, y:30}))
+        }
+        this.gameObjects = [this.ball, this.paddle, ...bricks];
 
         new InputHandler(this.paddle);
     }
