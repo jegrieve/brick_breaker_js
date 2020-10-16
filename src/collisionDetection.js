13369781cc464086ff0a226b45dd02401e3deeca
James Grieve <pocojames@hotmail.com>
2020-10-16 01:44:18 -0700

    add build levels

---
+++ src/collisionDetection.js
@@ -0,0 +1,14 @@
+export function detectCollision(ball, gameObject) {
+    let bottomOfBall = this.position.y + this.size;
+    let topOfPaddle = this.game.paddle.position.y;
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+    if(bottomOfBall >= topOfPaddle && 
+        this.position.x >= leftSideOfPaddle &&
+        this.position.x +this.size <= rightSideOfPaddle) {
+        this.speed.y = -this.speed.y;
+        this.position.y = this.game.paddle.position.y - this.size;
+    }
+};
+}
