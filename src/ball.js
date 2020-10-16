b33139fc9f19637b386df11f5c7c0dabb97a8943
James Grieve <pocojames@hotmail.com>
2020-10-16 01:54:44 -0700

    refactor collisions on ball class

--- src/ball.js
+++ src/ball.js
@@ -1,3 +1,5 @@
+import { detectCollision } from '/src/collisionDetection.js'
+
 export default class Ball {
     constructor(game) {
         this.image = document.getElementById("img_ball");
@@ -35,14 +37,7 @@ export default class Ball {
         };
 
         //check collision with paddle
-        let bottomOfBall = this.position.y + this.size;
-        let topOfPaddle = this.game.paddle.position.y;
-        let leftSideOfPaddle = this.game.paddle.position.x;
-        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
-
-        if(bottomOfBall >= topOfPaddle && 
-            this.position.x >= leftSideOfPaddle &&
-            this.position.x +this.size <= rightSideOfPaddle) {
+         if (detectCollision(this, this.game.paddle)) {
             this.speed.y = -this.speed.y;
             this.position.y = this.game.paddle.position.y - this.size;
         }
--- src/collisionDetection.js
+++ src/collisionDetection.js
@@ -1,14 +1,19 @@
 export function detectCollision(ball, gameObject) {
-    let bottomOfBall = this.position.y + this.size;
-    let topOfPaddle = this.game.paddle.position.y;
-    let leftSideOfPaddle = this.game.paddle.position.x;
-    let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+    let bottomOfBall = ball.position.y + ball.size;
+    let topOfBall = ball.position.y;
 
-    if(bottomOfBall >= topOfPaddle && 
-        this.position.x >= leftSideOfPaddle &&
-        this.position.x +this.size <= rightSideOfPaddle) {
-        this.speed.y = -this.speed.y;
-        this.position.y = this.game.paddle.position.y - this.size;
+    let topOfObject = gameObject.position.y;
+    let leftSideOfObject = gameObject.position.x;
+    let rightSideOfObject = gameObject.position.x + gameObject.width;
+    let bottomOfObject = gameObject.position.y + gameObject.height;
+
+    if(bottomOfBall >= topOfObject&& 
+        topOfBall <= bottomOfObject &&
+        ball.position.x >= leftSideOfObject&&
+        ball.position.x + ball.size <= rightSideOfObject
+        ) {
+            return true;
+        } else {
+           return false;
+       };
     }
-};
-}
