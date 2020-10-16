c14569895a2929c95f2834fbfb0c16ba053f7108
James Grieve <pocojames@hotmail.com>
2020-10-16 00:50:38 -0700

    add game class and refactor component classes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import Paddle from './paddle'
+import Game from "/src/game.js";
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-ctx.clearRect(0, 0, 800, 600);
+let lastTime = 0;
 
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+game.start();
 
-paddle.draw(ctx);
+function gameLoop(timestamp) { //runs every frame
+    let deltaTime = timestamp - lastTime;//calculate how much time has passed
+    lastTime = timestamp;
+
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);//clears screen
+
+    game.update(deltaTime);
+    game.draw(ctx);
+
+    requestAnimationFrame(gameLoop);//calls gameloop again with next frames timestamp.
+}
+
+requestAnimationFrame(gameLoop);
